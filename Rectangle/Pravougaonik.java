3fb1a11bf7020ab4e9bdbfa9218656152300be92
Byte-Block <55253959+Byte-Block@users.noreply.github.com>
2019-12-25 16:39:56 +0100

    Add files via upload

---
+++ Rectangle/Pravougaonik.java
@@ -0,0 +1,44 @@
+package paketDomaci15;
+
+public class Pravougaonik {
+	private double a;
+	private double b;
+	private String ime;
+	
+	public Pravougaonik (double a, double b, String ime) {
+		this.a = a;
+		this.b = b;
+		this.ime = ime;
+	}
+	public Pravougaonik (double a, String ime) {
+		this.a = a;
+		this.b = a;
+		this.ime = ime;
+	}
+	public double getA() {
+		return a;
+	}
+	public double getB() {
+		return b;
+	}
+	public String getIme ( ) {
+		return ime;
+	}
+	public double getObim () {
+		double Obim = 2 * a + 2 * b; 		
+		return Obim;
+	}
+	public double getPovrsina () {
+		double Povrsina = a * b;
+		return Povrsina;
+	}
+	public String ispisiPravougaonik () {
+		String ispis = "";
+		ispis = ispis + "Pravougaonik (Ime): "+ ime;
+		ispis = ispis + "\nDuzina prve stranice je "+a;
+		ispis = ispis + "\nDuzina druge stranice je "+b;
+		ispis = ispis + "\nPovrsina pravougaonika je "+getPovrsina();
+		ispis = ispis + "\nObim pravougaonika je "+getObim();
+		return ispis;
+	}
+}
